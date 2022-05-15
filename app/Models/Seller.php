88a05b48da5c52fa4d906167196c397c1dbe4ff6
Milan <milansto80@gmail.com>
2022-05-16 01:29:27 +0200

    adding some controllers and some relations

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class Seller extends Model
+class Seller extends User
 {
     use HasFactory;
+
+    public function products() {
+        return $this->hasMany(Product::class)
+    }
 }
